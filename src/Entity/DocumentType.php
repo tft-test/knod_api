573dcc19b09a0419aa9dcdddbfb21fbcae7a6232
KALUMVUATI Duramana <duramana.kalumvuati@epitech.eu>
2022-04-08 15:43:35 +0200

    created DocumentType entity

---
+++ src/Entity/DocumentType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\DocumentTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DocumentTypeRepository::class)]
+#[ApiResource]
+class DocumentType
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
