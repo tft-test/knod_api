0c32d2cc1ef7c9b0f3462b6b60511138fa20a143
Aimé ALOGNIKOU <48490387+alkomaJunior@users.noreply.github.com>
2022-04-09 10:04:19 +0200

    [#5] Creation entites and applied phpcs rules

---
+++ src/Entity/Company.php
@@ -0,0 +1,79 @@
+<?php /* @noinspection PhpPropertyOnlyWrittenInspection */
+
+/*
+ * This file describes the company resources.
+ */
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CompanyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @access public
+ *
+ * @version 0.1
+ */
+#[ORM\Entity(repositoryClass: CompanyRepository::class)]
+#[ApiResource]
+class Company
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $siret;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSiret(): ?string
+    {
+        return $this->siret;
+    }
+
+    /**
+     * @param string $siret
+     *
+     * @return $this
+     */
+    public function setSiret(string $siret): self
+    {
+        $this->siret = $siret;
+
+        return $this;
+    }
+}
