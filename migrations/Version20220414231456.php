56fc97bf24dac77b11d974ed3fced78ce0b9e4ea
KALUMVUATI Duramana <duramana.kalumvuati@epitech.eu>
2022-04-15 01:17:12 +0200

    .

--- migrations/Version20220414231456.php
+++ migrations/Version20220414231456.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220414225155 extends AbstractMigration
+final class Version20220414231456 extends AbstractMigration
 {
     public function getDescription(): string
     {
