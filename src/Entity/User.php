cc9113ba97a9efad9329ffcbf73d0263f101c8fd
KALUMVUATI Duramana <duramana.kalumvuati@epitech.eu>
2022-06-10 21:02:12 +0200

    validation for User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints\Valid;
